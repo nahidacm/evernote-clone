c78d76fe5183337bf6cd7bd768997083e54f4f09
Mahmudur Rahman <mahmudur.rahaman@sslwireless.com>
2020-11-14 20:09:43 +0600

    Created Navbar and form

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import useInput from '../../customhook/useInput'
 
 const Form = () => {
+    const [title, bindTitle, resetTitle] = useInput()
+    const [content, bindContent, resetContent] = useInput()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({title, content})
+        resetTitle()
+        resetContent()
+    }
     return (
         <div className="section">
             <form action="">
                 <h5 className="gray-text">New Note</h5>
 
-                    <div class="input-field">
-                        <input id="note_title" type="text" class="validate" />
-                        <label class="active" for="note_title">Note Title</label>
+                    <div className="input-field">
+                        <input id="note_title" type="text" className="validate" {...bindTitle} />
+                        <label className="active" for="note_title">Note Title</label>
                     </div>
                     <div className="input-field">
-                        <textarea id="note_content" class="materialize-textarea"></textarea>
+                        <textarea id="note_content" className="materialize-textarea" {...bindContent}></textarea>
                         <label for="note_content">Note Content</label>
                     </div>
+                    <button className="btn btn-green">Add</button>
 
             </form>
         </div>
